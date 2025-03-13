92a43f32dee855f8df4f4c1a41b1056974872d67
Nick Baker <nbakerd@amazon.com>
2025-03-13 22:46:12 +0000

    feat(nvidia): install nvidia dependencies from source
    cr: https://code.amazon.com/reviews/CR-187124995

--- templates/al2023/provisioners/install-nvidia-driver.sh
+++ templates/al2023/provisioners/install-nvidia-driver.sh
@@ -8,55 +8,7 @@ if [ "$ENABLE_ACCELERATOR" != "nvidia" ]; then
   exit 0
 fi
 
-#Detect Isolated partitions
-function is-isolated-partition() {
-  PARTITION=$(imds /latest/meta-data/services/partition)
-  NON_ISOLATED_PARTITIONS=("aws" "aws-cn" "aws-us-gov")
-  for NON_ISOLATED_PARTITION in "${NON_ISOLATED_PARTITIONS[@]}"; do
-    if [ "${NON_ISOLATED_PARTITION}" = "${PARTITION}" ]; then
-      return 1
-    fi
-  done
-  return 0
-}
-
-function rpm_install() {
-  local RPMS=($@)
-  echo "Pulling and installing local rpms from s3 bucket"
-  for RPM in "${RPMS[@]}"; do
-    aws s3 cp --region ${BINARY_BUCKET_REGION} s3://${BINARY_BUCKET_NAME}/rpms/${RPM} ${WORKING_DIR}/${RPM}
-    sudo dnf localinstall -y ${WORKING_DIR}/${RPM}
-  done
-}
-
-echo "Installing NVIDIA ${NVIDIA_DRIVER_MAJOR_VERSION} drivers..."
-
-################################################################################
-### Add repository #############################################################
-################################################################################
-# Determine the domain based on the region
-if is-isolated-partition; then
-  sudo dnf install -y nvidia-release
-  sudo sed -i 's/$dualstack//g' /etc/yum.repos.d/amazonlinux-nvidia.repo
-
-  rpm_install "opencl-filesystem-1.0-5.el7.noarch.rpm" "ocl-icd-2.2.12-1.el7.x86_64.rpm"
-
-else
-  if [[ $AWS_REGION == cn-* ]]; then
-    DOMAIN="nvidia.cn"
-  else
-    DOMAIN="nvidia.com"
-  fi
-
-  if [ -n "${NVIDIA_REPOSITORY:-}" ]; then
-    sudo dnf config-manager --add-repo ${NVIDIA_REPOSITORY}
-  else
-    sudo dnf config-manager --add-repo https://developer.download.${DOMAIN}/compute/cuda/repos/amzn2023/$(uname -m)/cuda-amzn2023.repo
-  fi
-  sudo dnf config-manager --add-repo https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
-  # update all current .repo sources to enable gpgcheck
-  sudo dnf config-manager --save --setopt=*.gpgcheck=1
-fi
+echo "Installing NVIDIA drivers..."
 
 ################################################################################
 ### Install drivers ############################################################
@@ -64,75 +16,127 @@ fi
 sudo mv ${WORKING_DIR}/gpu/gpu-ami-util /usr/bin/
 sudo mv ${WORKING_DIR}/gpu/kmod-util /usr/bin/
 
-sudo mkdir -p /etc/dkms
-echo "MAKE[0]=\"'make' -j$(grep -c processor /proc/cpuinfo) module\"" | sudo tee /etc/dkms/nvidia.conf
+sudo dnf -y install dkms
 sudo dnf -y install kernel-modules-extra
 
-function archive-open-kmods() {
-  if is-isolated-partition; then
-    sudo dnf -y install "kmod-nvidia-open-dkms-${NVIDIA_DRIVER_MAJOR_VERSION}.*"
-  else
-    sudo dnf -y module install nvidia-driver:${NVIDIA_DRIVER_MAJOR_VERSION}-open
-  fi
-  # The DKMS package name differs between the RPM and the dkms.conf in the OSS kmod sources
-  # TODO: can be removed if this is merged: https://github.com/NVIDIA/open-gpu-kernel-modules/pull/567
-  sudo sed -i 's/PACKAGE_NAME="nvidia"/PACKAGE_NAME="nvidia-open"/g' /var/lib/dkms/nvidia-open/$(kmod-util module-version nvidia-open)/source/dkms.conf
+function install-nvidia-driver() {
+  local runfile_url=$1
+  local workdir=NVIDIA-Linux
 
-  sudo kmod-util archive nvidia-open
+  sudo mkdir -p ${workdir}
+  pushd ${workdir}
 
-  KMOD_MAJOR_VERSION=$(sudo kmod-util module-version nvidia-open | cut -d. -f1)
-  SUPPORTED_DEVICE_FILE="${WORKING_DIR}/gpu/nvidia-open-supported-devices-${KMOD_MAJOR_VERSION}.txt"
-  sudo mv "${SUPPORTED_DEVICE_FILE}" /etc/eks/
+  sudo curl -O ${runfile_url}
 
-  sudo kmod-util remove nvidia-open
+  # should only be one file for the runfile.
+  local package=$(ls)
+  sudo sh ${package} --extract-only
+  sudo rm -rf ${package}
+  # should only be one directory for the extracted contents.
+  cd $(ls)
 
-  if is-isolated-partition; then
-    sudo dnf -y remove --all nvidia-driver
-    sudo dnf -y remove --all "kmod-nvidia-open*"
-  else
-    sudo dnf -y module remove --all nvidia-driver
-    sudo dnf -y module reset nvidia-driver
-  fi
-}
+  # we want differet names for the dkms module between proprietary and open
+  # builds. this enables us to load them by name at boot time.
+  sudo sed -i 's/PACKAGE_NAME="nvidia"/PACKAGE_NAME="nvidia-open"/g' kernel-open/dkms.conf
 
-function archive-proprietary-kmod() {
-  if is-isolated-partition; then
-    sudo dnf -y install "kmod-nvidia-latest-dkms-${NVIDIA_DRIVER_MAJOR_VERSION}.*"
-  else
-    sudo dnf -y module install nvidia-driver:${NVIDIA_DRIVER_MAJOR_VERSION}-dkms
-  fi
+  # proprietary kmod installation
+  sudo ./nvidia-installer --kernel-module-type proprietary --dkms --silent || sudo cat /var/log/nvidia-installer.log
   sudo kmod-util archive nvidia
   sudo kmod-util remove nvidia
+
+  # open kmod installation
+  sudo ./nvidia-installer --kernel-module-type open --dkms --silent || sudo cat /var/log/nvidia-installer.log
+  sudo kmod-util archive nvidia-open
+  sudo kmod-util remove nvidia-open
+  
+  # uninstall everything before doing a clean install of just the nvidia
+  # userspace drivers.
+  sudo ./nvidia-installer --uninstall --silent
+  sudo ./nvidia-installer --no-kernel-modules --silent
+
+  # assemble the list of supported nvidia devices for the open kernel modules
+  echo "# This file was generated from supported-gpus/supported-gpus.json\n$(sed -e 's/^/# /g' supported-gpus/LICENSE)" \
+    | sudo tee -a /etc/eks/nvidia-supported-devices.txt
+
+  cat supported-gpus/supported-gpus.json \
+    | jq -r '.chips[] | select(.features[] | contains("kernelopen")) | "\(.devid) \(.name)"' \
+    | sort -u \
+    | sudo tee -a /etc/eks/nvidia-supported-devices.txt
+
+  popd
+
+  sudo rm -rf $workdir
 }
 
-archive-open-kmods
-archive-proprietary-kmod
+function install-fabricmanager() {
+  local asset_url=$1
+
+  sudo mkdir -p staging
+  sudo curl ${asset_url} | sudo tar -J -xvf - --strip-components 1 -C staging
+
+  pushd staging
+  # see: https://github.com/NVIDIA/yum-packaging-fabric-manager/blob/main/fabricmanager.spec#L83-L109
+  sudo rsync -alvK bin/ /usr/bin/
+  sudo rsync -alvK sbin/ /usr/sbin/
+  sudo rsync -alvK include/ /usr/include/
+  sudo rsync -alvK lib/ /usr/lib/
+  sudo rsync -alvK systemd/ /usr/lib/systemd/system/
+  sudo rsync -alvK share/ /usr/share/
+  sudo rsync -alvK etc/ /usr/share/nvidia/nvswitch/
+  popd
+
+  sudo rm -rf staging
+}
+
+function install-imex() {
+  local asset_url=$1
+
+  sudo mkdir -p staging
+  sudo curl ${asset_url} | sudo tar -J -xvf - --strip-components 1 -C staging
+
+  pushd staging
+  sudo rsync -alvK usr/bin/ /usr/bin/
+  sudo rsync -alvK lib/ /usr/lib/
+  sudo rsync -alvK etc/ /usr/etc/
+  popd
+
+  sudo rm -rf staging
+}
+
+function install-nvidia-container-toolkit() {
+  local asset_url=$1
+
+  sudo mkdir -p staging
+  sudo curl -L ${asset_url} | sudo tar -zxvf - --strip-components 4 -C staging
+
+  pushd staging
+  sudo dnf install -y ./libnvidia-container1-*
+  sudo dnf install -y ./libnvidia-container-tools-*
+  sudo dnf install -y ./nvidia-container-toolkit-base-*
+  sudo dnf install -y ./nvidia-container-toolkit-*
+  popd
+
+  sudo rm -rf staging
+}
+
+sudo dnf install -y rsync
+
+install-nvidia-driver             ${NVIDIA_DRIVER_RUNFILE_URL}
+install-fabricmanager             ${NVIDIA_FABRICMANAGER_URL}
+install-nvidia-container-toolkit  ${NVIDIA_CONTAINER_TOOLKIT_URL}
+install-imex                      ${NVIDIA_IMEX_URL}
+
+sudo dnf remove -y rsync
+
+sudo systemctl enable nvidia-fabricmanager
+sudo systemctl enable nvidia-persistenced
 
 ################################################################################
 ### Prepare for nvidia init ####################################################
 ################################################################################
-
 sudo mv ${WORKING_DIR}/gpu/nvidia-kmod-load.sh /etc/eks/
 sudo mv ${WORKING_DIR}/gpu/nvidia-kmod-load.service /etc/systemd/system/nvidia-kmod-load.service
 sudo mv ${WORKING_DIR}/gpu/set-nvidia-clocks.service /etc/systemd/system/set-nvidia-clocks.service
 sudo systemctl daemon-reload
 sudo systemctl enable nvidia-kmod-load.service
 sudo systemctl enable set-nvidia-clocks.service
-
-################################################################################
-### Install other dependencies #################################################
-################################################################################
-sudo dnf -y install nvidia-fabric-manager
-sudo dnf -y install "nvidia-imex-${NVIDIA_DRIVER_MAJOR_VERSION}.*"
-
-# NVIDIA Container toolkit needs to be locally installed for isolated partitions, also install NVIDIA-Persistenced
-if is-isolated-partition; then
-  sudo dnf -y install nvidia-container-toolkit
-  sudo dnf -y install "nvidia-persistenced-${NVIDIA_DRIVER_MAJOR_VERSION}.*"
-  sudo dnf -y install "nvidia-driver"
-else
-  sudo dnf -y install nvidia-container-toolkit
-fi
-
-sudo systemctl enable nvidia-fabricmanager
-sudo systemctl enable nvidia-persistenced
